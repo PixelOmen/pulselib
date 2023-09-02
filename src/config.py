28499dfbbb877faa583e348d511ad5e546bc2099
Eman <eacosta@roundabout.com>
2023-09-01 19:27:15 -0700

    Added probe url to config

--- src/config.py
+++ src/config.py
@@ -4,6 +4,7 @@ class Config:
     TRX_QUERY_URL: str = "http://xytechapp01:11000/api/v1/database/REI_LIVE/JmTrxList"
     WO_URL = "http://xytechapp01:11000/api/v1/database/REI_LIVE/JmWorkOrder"
     WO_QUERY_URL = "http://xytechapp01:11000/api/v1/database/REI_LIVE/JmWorkOrderList"
-    ASSET_SESSION_URL = "http://xytechapp01:11000/api/v1/database/REI_LIVE/LibMaster_ImSession_Related"
+    ASSET_SESSION_URL = "http://xytechapp01:11000/api/v1/database/REI_LIVE/LibMaster_ImSession_Related",
+    MEDIAINFO_URL = "http://10.0.20.96:4020/api/probe"
 
 CONFIG = Config()
