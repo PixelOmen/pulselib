d14891c90280468dd8d7d5a45fda568ae9505316
Eman <eacosta@roundabout.com>
2023-09-06 19:50:01 -0700

    Started SpecInterface for asset probing

--- src/asset/probing.py
+++ src/asset/probing.py
@@ -1,5 +1,5 @@
 from dataclasses import dataclass
-from mediaprobe import MediaAttributes
+from mediaprobe import MediaProbe
 
 # "error": "A matching value could not be found for the custom dropdown field REI_field_19; Value: 42 bit.\r\n"
 
@@ -37,31 +37,32 @@ FRAMERATE_ENUM: dict[str, int] = {
 }
 
 @dataclass
-class ProbeMethod:
+class SpecMethod:
     spec: str
     track: str=""
     field: str=""
     probetype: str="simple"
+    value: str=""
 
-SPEC_PROBE_MAP: dict[str, ProbeMethod] = {
-    "length": ProbeMethod("length", "General", "Duration"),
-    "resolution": ProbeMethod("resolution", probetype="complex"),
-    "dropframe": ProbeMethod("dropframe", probetype="complex"),
-    "color_space": ProbeMethod("color_space", "Video", "colour_primaries"),
-    "eotf": ProbeMethod("eotf", "Video", "transfer_characteristics"),
-    "matrix": ProbeMethod("matrix", "Video", "matrix_coefficients"),
-    "chroma_sub": ProbeMethod("chroma_sub", "Video", "ChromaSubsampling"),
-    "frame_rate": ProbeMethod("frame_rate", "Video", "FrameRate"),
-    "scan_type": ProbeMethod("scan_type", "Video", "ScanType"),
-    "video_codec": ProbeMethod("video_codec", "Video", "Format"),
-    "video_profile": ProbeMethod("video_profile", "Video", "Format_Profile"),
-    "video_bitrate": ProbeMethod("video_bitrate", "Video", "BitRate"),
-    "video_bitdepth": ProbeMethod("video_bitdepth", "Video", "BitDepth"),
-    "video_bitrate_mode": ProbeMethod("video_bitrate_mode", "Video", "BitRate_Mode"),
-    "audio_bitrate": ProbeMethod("audio_bitrate", "Audio", "BitRate"),
-    "audio_bitdepth": ProbeMethod("audio_bitdepth", "Audio", "BitDepth"),
-    "audio_bitrate_mode": ProbeMethod("audio_bitrate_mode", "Audio", "BitRate_Mode"),
-    "audio_samplerate": ProbeMethod("audio_samplerate", "Audio", "SamplingRate")
+SPEC_PROBE_MAP: dict[str, tuple[str, str, str]] = {
+    "length": ("General", "Duration", "simple"),
+    "resolution": ("", "simple", "complex"),
+    "dropframe": ("", "simple", "complex"),
+    "color_space": ("Video", "colour_primaries", "simple"),
+    "eotf": ("Video", "transfer_characteristics", "simple"),
+    "matrix": ("Video", "matrix_coefficients", "simple"),
+    "chroma_sub": ("Video", "ChromaSubsampling", "simple"),
+    "frame_rate": ("Video", "FrameRate", "simple"),
+    "scan_type": ("Video", "ScanType", "simple"),
+    "video_codec": ("Video", "Format", "simple"),
+    "video_profile": ("Video", "Format_Profile", "simple"),
+    "video_bitrate": ("Video", "BitRate", "simple"),
+    "video_bitdepth": ("Video", "BitDepth", "simple"),
+    "video_bitrate_mode": ("Video", "BitRate_Mode", "simple"),
+    "audio_bitrate": ("Audio", "BitRate", "simple"),
+    "audio_bitdepth": ("Audio", "BitDepth", "simple"),
+    "audio_bitrate_mode": ("Audio", "BitRate_Mode", "simple"),
+    "audio_samplerate": ("Audio", "SamplingRate", "simple")
 }
 
 SPEC_FIELD_MAP: dict[str, str] = {
@@ -84,3 +85,29 @@ SPEC_FIELD_MAP: dict[str, str] = {
     "audio_bitrate_mode": "REI_field_25",
     "audio_samplerate": "REI_field_22"
 }
+
+
+class SpecInterface:
+    def __init__(self, probe: MediaProbe):
+        self.probe = probe
+        self.specs: list[SpecMethod] = self._methods()
+
+    def _methods(self) -> list[SpecMethod]:
+        return [SpecMethod(spec, *SPEC_PROBE_MAP[spec]) for spec in SPEC_PROBE_MAP]
+
+    def _find(self) -> None:
+        for method in self.specs:
+            if method.probetype == "simple":
+                self._simple(method)
+            elif method.probetype == "complex":
+                self._complex(method)
+
+    def _simple(self, method: SpecMethod) -> None:
+        for track in self.probe.fulljson["tracks"]:
+            if track["@type"] == method.track:
+                value = track.get(method.field)
+                if value:
+                    method.value = value
+
+    def _complex(self, method: SpecMethod) -> None:
+        pass
