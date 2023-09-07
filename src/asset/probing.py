2a05b3577a41444decaebc4eaf7d7aa7670813a7
Eman <eacosta@roundabout.com>
2023-09-07 13:45:58 -0700

    Renamed SpecMethod->SpecInfo, added input and output values

--- src/asset/probing.py
+++ src/asset/probing.py
@@ -62,12 +62,13 @@ FRAMERATE_ENUM: dict[str, int] = {
 }
 
 @dataclass
-class SpecMethod:
+class SpecInfo:
     spec: str
     track: str=""
     field: str=""
     probetype: str="simple"
-    value: str=""
+    inputvalue: str=""
+    outputvalue: str=""
 
 SPEC_PROBE_MAP_SIMPLE: dict[str, tuple[str, str, str]] = {
     "color_space": ("Video", "colour_primaries", "simple"),
@@ -99,33 +100,41 @@ SPEC_PROBE_MAP_COMPLEX: list[str] = [
 class SpecInterface:
     def __init__(self, probe: "MediaProbe"):
         self.probe = probe
-        self.specs: list[SpecMethod] = self._methods()
-        self._notfound: list[SpecMethod] = []
+        self.all: list[SpecInfo] = self._create_specinfo()
+        self.found: list[SpecInfo] = []
+        self.notfound: list[SpecInfo] = []
         self._find()
 
-    def _methods(self) -> list[SpecMethod]:
-        simple = [SpecMethod(spec, *SPEC_PROBE_MAP_SIMPLE[spec]) for spec in SPEC_PROBE_MAP_SIMPLE]
-        complex = [SpecMethod(spec, probetype="complex") for spec in SPEC_PROBE_MAP_COMPLEX]
+    def _create_specinfo(self) -> list[SpecInfo]:
+        simple = [SpecInfo(spec, *SPEC_PROBE_MAP_SIMPLE[spec]) for spec in SPEC_PROBE_MAP_SIMPLE]
+        complex = [SpecInfo(spec, probetype="complex") for spec in SPEC_PROBE_MAP_COMPLEX]
         return simple + complex
 
+    def _add_found(self, method: SpecInfo) -> None:
+        self.found.append(method)
+
+    def _add_notfound(self, method: SpecInfo) -> None:
+        self.notfound.append(method)
+
     def _find(self) -> None:
-        for method in self.specs:
+        for method in self.all:
             if method.probetype == "simple":
                 self._simple(method)
             elif method.probetype == "complex":
                 self._complex(method)
 
-    def _simple(self, method: SpecMethod) -> None:
+    def _simple(self, method: SpecInfo) -> None:
         for track in self.probe.fulljson["tracks"]:
             if track["@type"] == method.track:
                 value = track.get(method.field)
                 if value:
-                    method.value = value
+                    method.inputvalue = value
+                    self._add_found(method)
                 else:
-                    self._notfound.append(method)
+                    self._add_notfound(method)
                 break
 
-    def _complex(self, method: SpecMethod) -> None:
+    def _complex(self, method: SpecInfo) -> None:
         match method.spec:
             case "length":
                 self._length_spec(method)
@@ -144,28 +153,37 @@ class SpecInterface:
             return True
         return False
             
-    def _length_spec(self, method: SpecMethod) -> None:
+    def _length_spec(self, method: SpecInfo) -> None:
         duration = self.probe.duration()
         if not duration:
-            self._notfound.append(method)
+            self._add_notfound(method)
             return
         
+        method.inputvalue = duration
+
         fps_str = self.probe.fps()
         if not fps_str:
-            method.value = duration
+            method.outputvalue = duration
+            self._add_found(method)
             return
         
         fps_int = round(float(fps_str))
         hrminsec = [0, 0, float(duration)]
         frames = tclib3.ms_to_frames(hrminsec, fps_int, True)
-        method.value = tclib3.frames_to_tc(frames-1, fps_int, self._is_df())
+        method.outputvalue = tclib3.frames_to_tc(frames-1, fps_int, self._is_df())
+        self._add_found(method)
 
-    def _resolution_spec(self, method: SpecMethod) -> None:
+    def _resolution_spec(self, method: SpecInfo) -> None:
         resolution_str = self.probe.resolution(asint=False)
         if resolution_str is None:
-            self._notfound.append(method)
+            self._add_notfound(method)
             return
-        method.value = f"{resolution_str[0]} x {resolution_str[1]}"
+        method.inputvalue = f"{resolution_str[0]},{resolution_str[1]}"
+        method.outputvalue = f"{resolution_str[0]} x {resolution_str[1]}"
+        self._add_found(method)
 
-    def _dropframe_spec(self, method: SpecMethod) -> None:
-        method.value = "True" if self._is_df() else ""
+    def _dropframe_spec(self, method: SpecInfo) -> None:
+        is_df = self._is_df()
+        method.inputvalue = "True" if is_df else ""
+        method.outputvalue = "Y" if is_df else "N"
+        self._add_found(method)
