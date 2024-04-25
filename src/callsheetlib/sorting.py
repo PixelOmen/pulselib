cc763440cdde2336dedfc1f93b5ac6eb3d2c6255
Eman <cagefury@gmail.com>
2024-04-25 13:46:31 -0700

    Added workordergroups method to RoomGroup class

--- src/callsheetlib/sorting.py
+++ src/callsheetlib/sorting.py
@@ -148,7 +148,6 @@ class JobGroup:
 
 
 class RoomGroup:
-
     @staticmethod
     def format_date(datestr: str, fulldate: bool=False) -> str:
         date = datetime.strptime(datestr, "%Y-%m-%dT%H:%M:%S")
@@ -172,15 +171,10 @@ class RoomGroup:
             blocks.append(WorkOrderBlock(header_rows, [datarow], is_maintenance=True))
         return blocks
     
-    def __init__(self, room_trx: Transaction, jobgroups: list[JobGroup],
-                 table_headers: list[str] | None = None):
+    def __init__(self, room_trx: Transaction, jobgroups: list[JobGroup]):
         # Duplicates are removed in table_data() and block_data() due to separate billing trxs
         self.room_trx = room_trx
         self.jobgroups: list[JobGroup] = jobgroups
-        if table_headers is None:
-            self.table_headers = ["Workorder", "Begin", "End", "Personnel", "Title", "Company", "SAN", "Notes"]
-        else:
-            self.table_headers = table_headers
         self.block_types = {
             ReportEnum.scheduling: self._scheduling_block,
             ReportEnum.operations: self._operations_block,
@@ -253,14 +247,6 @@ class RoomGroup:
             rows.append([person.wo, begin, end, person.name, person.job_desc,
                          person.company, san, person.frontdeskinfo])
         return rows
-
-    # ----- Deprecated -----
-    # def _single_table_row(self, trx: Transaction, san: str="", is_room: bool=False) -> list[str]:
-    #     begin = self._format_date(trx.begin)
-    #     end = self._format_date(trx.end)
-    #     person = "" if is_room else trx.name
-    #     return [trx.wo, begin, end, person, trx.job_desc,
-    #             trx.company, san, trx.frontdeskinfo]
 
     def _scheduling_block(self, jobgroup: JobGroup, workgroup: WorkorderGroup, room_trx: Transaction | None=None) -> WorkOrderBlock:
         if room_trx is None:
@@ -519,25 +505,22 @@ class RoomGroup:
         blocks.sort()
         return self._remove_duplicates(blocks)
     
-    # ----- Deprecated -----
-    # def table_data(self) -> list[DataRow]:
-    #     rows: list[DataRow] = []
-    #     for jobgroup in self.jobgroups:
-    #         for workgroup in jobgroup.workordergroups:
-    #             if workgroup.room_trx is None or workgroup.room_trx.name != self.room_trx.name:
-    #                 continue
-    #             if workgroup.room_only:
-    #                 san = workgroup.sans[0].name if workgroup.sans else ""
-    #                 rows.append(DataRow(self._single_table_row(workgroup.room_trx, san, is_room=True), is_wo_header=True))
-    #             else:
-    #                 personnel = self._personnel_table_rows(workgroup)
-    #                 if personnel:
-    #                     rows.extend([DataRow(row, is_actor=False) for row in personnel])
-    #         if self.room_trx.group in SAG_GROUPS_ROOMS:
-    #             for actorlist in jobgroup.actors_by_wo.values():
-    #                 rows.extend([DataRow(self._single_table_row(actor), is_actor=True) for actor in actorlist])
-    #     rows.sort()
-    #     return self._remove_duplicates(rows)
+    def workordergroups(self) -> list[WorkorderGroup]:
+        worders = []
+        for jobgroup in self.jobgroups:
+            for workgroup in jobgroup.workordergroups:
+                if (workgroup.room_trx is None or
+                    workgroup.room_trx.name != self.room_trx.name):
+                    continue
+                worders.append(workgroup)
+            if self.room_trx.group in SAG_GROUPS_ROOMS:
+                for actorgroup in jobgroup.actorgroups:
+                    if self.room_trx.name.lower().startswith("adr"):
+                        wo_desc = actorgroup.personnel[0].wo_desc
+                        if not self._is_adr_match(self.room_trx.name, wo_desc):
+                            continue
+                        worders.append(actorgroup)
+        return worders
     
 
 
@@ -551,20 +534,6 @@ class ResourceGroups:
         self.equipment: list[Equipment] = []
         self.other: dict[str, list[Transaction]] = {}
         self._split_into_groups()
-
-    # def _filter_equipment(self) -> tuple[list[Transaction], list[Transaction]]:
-    #     equipment: list[Transaction] = []
-    #     non_equipment: list[Transaction] = []
-    #     for trx in self.trxlist:
-    #         resource = trx.get_resource()
-    #         if resource is None:
-    #             raise ValueError(f"Transaction does not have a resource: {trx.name}")
-    #         if resource.type.lower() == "equipment":
-    #             equipment.append(trx)
-    #         else:
-    #             non_equipment.append(trx)
-    #     return equipment, non_equipment
-
 
     def _split_into_groups(self) -> None:
         # temp dicts to avoid multiple objects with same name
